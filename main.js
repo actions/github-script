0f28aeff3d800c5c8e17c9293e128f733bc20cb1
Jonathan Clem <jclem@github.com>
2019-09-05 11:29:31 -0400

    Pass input options to GitHub constructor

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ main().catch(handleError)
 
 async function main() {
   const AsyncFunction = Object.getPrototypeOf(async () => {}).constructor
-  const script = core.getInput('script', {required: true})
   const token = core.getInput('github-token', {required: true})
+  const debug = core.getInput('debug')
+  const userAgent = core.getInput('user-agent')
+  const previews = core.getInput('previews')
+  const opts = {}
+  if (debug) opts.log = console
+  if (userAgent) opts.userAgent = userAgent
+  if (previews) opts.previews = previews
+  const client = new GitHub(token, opts)
+  const script = core.getInput('script', {required: true})
   const fn = new AsyncFunction('github', 'context', script)
-  const client = new GitHub(token)
   const result = await fn(client, context)
   core.setOutput('result', JSON.stringify(result))
 }
