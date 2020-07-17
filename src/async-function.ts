002432b25efaba8a43537e1c1ce33865aa46556b
Jonathan Clem <jclem@github.com>
2020-07-17 11:45:02 -0400

    Remove unnecessary type

--- src/async-function.ts
+++ src/async-function.ts
@@ -1,12 +1,10 @@
-type AsyncFunction<A, R> = (args: A) => Promise<R>
-
 export async function callAsyncFunction<A = {}, R = unknown>(
   args: A,
   source: string
 ): Promise<R> {
   const argsKeys = Object.keys(args).join(',')
 
-  const wrappedFunction: AsyncFunction<A, R> = eval(`async({${argsKeys}}) => {
+  const wrappedFunction: (args: A) => Promise<R> = eval(`async({${argsKeys}}) => {
     ${source}
   }`)
 
