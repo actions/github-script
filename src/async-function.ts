5f39025188016c6076168de8651b0da37a53b68a
Jonas Bovin <jonas.bovin@schneider-electric.com>
2022-11-22 11:47:40 +0100

    Added se

--- src/async-function.ts
+++ src/async-function.ts
@@ -1,9 +1,10 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import {Context} from '@actions/github/lib/context'
-import {GitHub} from '@actions/github/lib/utils'
+import { Context } from '@actions/github/lib/context'
+import { GitHub } from '@actions/github/lib/utils'
 import * as glob from '@actions/glob'
 import * as io from '@actions/io'
+import * as se from './se'
 
 const AsyncFunction = Object.getPrototypeOf(async () => null).constructor
 
@@ -14,6 +15,7 @@ type AsyncFunctionArguments = {
   exec: typeof exec
   glob: typeof glob
   io: typeof io
+  se: typeof se
   require: NodeRequire
   __original_require__: NodeRequire
 }
